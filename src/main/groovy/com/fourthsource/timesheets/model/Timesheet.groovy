ac393ee2dcb7ad1afff4ff0cff1c3f3dd26b6b6b
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2013-08-28 23:48:48 -0500

    Started to map Timesheet entity. Still not sure how I will combine simplicity with flexibility

--- src/main/groovy/com/fourthsource/timesheets/model/Timesheet.groovy
+++ src/main/groovy/com/fourthsource/timesheets/model/Timesheet.groovy
@@ -1,11 +1,33 @@
 package com.fourthsource.timesheets.model
 
-import org.joda.time.LocalDate
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+import javax.persistence.Temporal
+import javax.persistence.TemporalType
 
+@Entity
+@Table(name = 'TIMESHEETS')
 class Timesheet {
 
+    @Id
+    @Column(name = 'TIMESHEET_ID')
+    @GeneratedValue
+    Integer id
+
+    @Column(name = 'WEEK_ENDING', nullable = false)
+    @Temporal(TemporalType.DATE)
+    Calendar weekEnding
+
+    @ManyToOne
+    @JoinColumn(name = 'RESOURCE_ID', nullable = false)
     Resource resource
-    LocalDate weekEnding
+
+
     List<BillableTime> billableTimes
 
 }
